a4790267bea16510270ea9a7857b156925f6c342
huihui12345678 <477909767@qq.com>
2019-03-25 02:23:32 +0800

    Add files via upload

---
+++ page/src/components/HelloWorld.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="hello">
+  	<div class="header">
+  		<div></div>
+  	</div>
+  	<div class="main">
+  			<div class="content1">
+  				<label>用户名：<input type="text" v-model="user"/></label>
+  				
+  			</div>
+  			<div class="content2">
+  				<label>密码：&nbsp;<input type="password" v-model="password1" /></label>
+  				
+  			</div>
+  		
+  	</div>
+  	<ul class="footer">
+  			<router-link  :to="{ path: '/IndexPage' }" active-class="login" tag="li" > 
+			<span >登录</span>
+		</router-link>
+  	</ul>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      user:'zhangsan',
+      password1:''
+    }
+  },
+  created (){
+ 
+  },
+  watch: {
+  	
+  },
+  beforeRouteLeave (to, from, next) {
+  	if(!this.password1||!this.user){
+  		next(false)
+  	}else{
+  		next()
+  	}
+    // 在当前路由改变，但是该组件被复用时调用
+    // 举例来说，对于一个带有动态参数的路径 /foo/:id，在 /foo/1 和 /foo/2 之间跳转的时候，
+    // 由于会渲染同样的 Foo 组件，因此组件实例会被复用。而这个钩子就会在这个情况下被调用。
+    // 可以访问组件实例 `this`
+  },
+  methods: {
+// login(){
+//  		this.$axios.get('../static/data.json')
+//.then(function (response) {
+//  console.log(response);
+//})
+//.catch(function (error) {
+//  console.log(error);
+//});
+// }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	ul {
+  list-style-type: none;
+  padding: 0;
+  
+}
+.hello{
+	border: 1px solid gainsboro;
+	border-radius: 5px;
+	height: 210px;
+	width: 300px;
+	margin: auto;
+	box-shadow: 2px 2px 2px 2px #f3f1f1;
+}
+.hello div{
+	width: 100%;
+}
+.header{
+	height: 30px;
+	background: #f3f1f1;
+}
+.header div{
+	width: 25px;
+	height: 25px;
+	
+}
+.content1{
+	height: 60px;
+	border-bottom:1px solid gainsboro;
+}
+.content2{
+	height: 60px;
+	border-bottom:1px solid gainsboro;
+}
+label{
+	  margin: 18px 0;
+    display: inline-block;
+}
+li{
+	    position: relative;
+    left: 40px;
+    background: #f3f1f1;
+    padding: 2px 0;
+    width: 70px;
+    border: 1px solid gainsboro;
+    border-radius:2px;
+    box-shadow: 2px 2px 2px 2px #f3f1f1;
+}
+</style>
---
+++ page/src/components/IndexPage.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="hello">
+  	<div class="header">
+  		<span></span>
+  	<span>用户名：</span><span>{{user}}</span>
+  		<span>所属部门：</span><span>{{department}}</span>
+  	</div>
+  	<div class="main">
+  			<div class="content1">
+  			
+  				<h5>{{msg}}</h5>
+  			</div>
+  			<div class="content2">
+  			
+  				
+  			</div>
+  			
+  			
+  		
+  	</div>
+  	<ul class="footer">
+  			<router-link  :to="{ path: '/' }" active-class="login" tag="li"> 
+			退出
+		</router-link>
+  	</ul>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '欢迎光临考试系统...',
+      user:'zhangsan',
+      department:''
+    }
+  },
+  created (){
+    		this.$axios.get('../static/data.json')
+  .then( (res)=> {
+    console.log(res);
+     
+     this.user=res.data.usercode
+     	this.department=res.data.comcode
+    
+//   this.$nextTick(function () {
+//  
+//     this.user=res.data.usercode
+//   	this.department=res.data.comcode
+//      // DOM 现在更新了
+//      // `this` 绑定到当前实例
+//      
+//    })
+  })
+  .catch( (error)=> {
+    console.log(error);
+  });
+  },
+  mounted(){
+ 
+  },
+  methods:{
+  
+  },
+	msg1(res){
+   
+   }
+   
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	ul {
+  list-style-type: none;
+  padding: 0;
+  
+}
+.hello{
+	border: 1px solid gainsboro;
+	border-radius: 5px;
+	height: 210px;
+	width: 300px;
+	margin: auto;
+	box-shadow: 2px 2px 2px 2px #f3f1f1;
+}
+.hello div{
+	width: 100%;
+}
+.header{
+	height: 30px;
+	background: #f3f1f1;
+}
+.header div{
+	width: 25px;
+	height: 25px;
+	
+}
+h5{
+	margin: 0;
+}
+.content1{
+	height: 60px;
+	/*border-bottom:1px solid gainsboro;*/
+}
+.content2{
+	height: 60px;
+	border-bottom:1px solid gainsboro;
+}
+
+li{
+	    position: relative;
+    left: 40px;
+    background: #f3f1f1;
+    padding: 2px 0;
+    width: 70px;
+    border: 1px solid gainsboro;
+    border-radius:2px;
+    box-shadow: 2px 2px 2px 2px #f3f1f1;
+}
+</style>
